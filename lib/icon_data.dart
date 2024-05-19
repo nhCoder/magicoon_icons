1c5e40112c31f7d46d83bc912de03420d09e1042
nhcoder <nhcoder0@outlook.com>
2024-05-20 00:00:42 +0500

    fixed icons not showing

---
+++ lib/icon_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class MagicoonIconData extends IconData {
+  const MagicoonIconData(super.codePoint)
+      : super(
+    fontFamily: 'MagicoonIcons',
+    fontPackage: 'magicoon_icons',
+  );
+}
